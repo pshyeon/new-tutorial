b70cc05c7f45b26c7bbf1154bbed3fd9160017eb
pshyeon <twinkle5078@naver.com>
2021-02-02 15:40:35 +0900

    Add Get customers REST API Modules

--- management/server.js
+++ management/server.js
@@ -6,7 +6,35 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extend:true}));
 
-app.get('/api/hello', (req,res)=>
-res.send({message:'Hello Express!'}));
+app.get('/api/customers', (req,res)=>{
+  res.send([
+    {
+      'id': '1',
+      'image': 'https://placeimg.com/64/64/1',
+      'name': '홍길동',
+      'birthday': '970703',
+      'gender': '남자',
+      'job': '대학생'
+  
+    },
+    {
+      'id': '2',
+      'image': 'https://placeimg.com/64/64/2',
+      'name': '윤상희',
+      'birthday': '970813',
+      'gender': '여자',
+      'job': '간호사'
+  
+    },
+    {
+      'id': '3',
+      'image': 'https://placeimg.com/64/64/3',
+      'name': '김혜미',
+      'birthday': '971221',
+      'gender': '여자',
+      'job': '공주님'
+  
+    }]);
+});
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
