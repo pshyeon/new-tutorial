ee86727a079bbcfa763f84082d893861c632bc2d
pshyeon <twinkle5078@naver.com>
2021-02-05 15:28:46 +0900

    Apply Material UI

--- management/src/App.js
+++ management/src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import Customer from './components/Customer';
 import logo from './logo.svg';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles=theme=>({
+  root:{
+    width:'100%',
+    marginTop:theme.spacing.unit * 3,
+    overflow:'auto'
+  },
+  table:{
+    minWidth:1080
+  }
+})
 
 const customers = [
   {
@@ -32,9 +50,23 @@ const customers = [
 
 class App extends Component {
   render() {
+    const {classes}=this.props;
     return (
-      <div>
-        {
+      <Paper className = {classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+        
+          <TableBody>
+          {
           customers.map(c=>{
             return(
               <Customer
@@ -47,10 +79,13 @@ class App extends Component {
             );
           })
           }
+          </TableBody>
+        </Table>
+        
 
-        </div>
+        </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
