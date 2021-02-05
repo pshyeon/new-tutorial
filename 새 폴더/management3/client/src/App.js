6105f9e9d04d03f01cdfa53c400cf63375f38156
pshyeon <twinkle5078@naver.com>
2021-02-05 16:45:19 +0900

    New Crete Customer.js

---
+++ 새 폴더/management3/client/src/App.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import Customer from './components/Customer';
+import logo from './logo.svg';
+import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles=theme=>({
+  root:{
+    width:'100%',
+    marginTop:theme.spacing.unit * 3,
+    overflow:'auto'
+  },
+  table:{
+    minWidth:1080
+  }
+})
+
+const customers = [
+  {
+    'id': '1',
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '박승현',
+    'birthday': '970703',
+    'gender': '여자',
+    'job': '대학생'
+  },
+  {
+    'id': '2',
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '윤상희',
+    'birthday': '970813',
+    'gender': '여자',
+    'job': '간호사'
+  },
+  {
+    'id': '3',
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '김혜미',
+    'birthday': '971221',
+    'gender': '여자',
+    'job': '공주님'
+  }
+]
+
+class App extends Component {
+  render() {
+    const {classes}=this.props;
+    return (
+      <Paper className = {classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+        
+          <TableBody>
+          {
+          customers.map(c=>{
+            return(
+              <Customer
+              id={c.id}
+              image={c.image}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+            );
+          })
+          }
+          </TableBody>
+        </Table>
+        
+
+        </Paper>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
