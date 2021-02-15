eb28eda549cad9d752c9afbb34ae63fbaa31bc30
pshyeon <twinkle5078@naver.com>
2021-02-15 16:48:16 +0900

    Create Customer Database Table & Connection

--- management/server.js
+++ management/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express =require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host:conf.host,
+  user: conf.user,
+  password : conf.password,
+  port : conf.port,
+  database : conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req,res)=>{
-  res.send([
-    {
-      'id': '1',
-      'image': 'https://placeimg.com/64/64/1',
-      'name': '박승현',
-      'birthday': '970703',
-      'gender': '여자',
-      'job': '대학생'
-    },
-    {
-      'id': '2',
-      'image': 'https://placeimg.com/64/64/2',
-      'name': '윤상희',
-      'birthday': '970813',
-      'gender': '여자',
-      'job': '간호사'
-    },
-    {
-      'id': '3',
-      'image': 'https://placeimg.com/64/64/3',
-      'name': '김혜미',
-      'birthday': '971221',
-      'gender': '여자',
-      'job': '공주님'
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
     }
-  ]);
+  );
 });
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
